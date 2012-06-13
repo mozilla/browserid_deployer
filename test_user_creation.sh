374855798fdf7247006acf9b69e88db550c35458
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-06-13 13:19:24 +0300

    a script to create selenium test user

---
+++ test_user_creation.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash 
+
+echo "INSERT INTO user(passwd) values('\$2a\$12\$BTrFTrD03.3CdqxbkUlBlOAlYMXW6vwuIo7Fg.SxPb3lzKOVRkHb2'); INSERT INTO email(user, address, type) VALUES(last_insert_id(), 'web.browserid@gmail.com', 'secondary');" | mysql -u browserid browserid
