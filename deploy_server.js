602c9c709e7a9e0310b660aa301f6a91de0208d1
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-09-17 14:29:53 +0300

    upgrade to awsbox 0.5.1

--- deploy_server.js
+++ deploy_server.js
@@ -96,7 +96,10 @@ Deployer.prototype._deployNewCode = function(cb) {
       self.emit('error', "can't npm install to prepare to run deploy_dev");
       return;
     }
-    var p = spawn(path.join(__dirname, 'deploy_dev.js'), [], { cwd: self._codeDir });
+    var p = spawn(path.join(__dirname, 'deploy_dev.js'), [], {
+      cwd: self._codeDir,
+      env: process.env
+    });
 
     p.stdout.on('data', splitAndEmit);
     p.stderr.on('data', splitAndEmit);
